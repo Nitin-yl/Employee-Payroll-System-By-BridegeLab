2757ac49eeea32e9b89780790a3e9f69fdb6b349
Nitin-Goyal12 <navgoyal321@gmail.com>
2026-02-18 14:25:50 +0530

    step 2 complated

--- modules/fileHandler.js
+++ modules/fileHandler.js
@@ -1,25 +1,22 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const filePath = path.join(__dirname, "..", "employees.json");
-
-// read data
+const filePath = path.join(__dirname, "../employees.json");
 async function read() {
   try {
-    const data = await fs.readFile(filePath, "utf-8");
+    const data = await fs.readFile(filePath, "utf8");
     return JSON.parse(data);
-  } catch (err) {
-    console.error("Error reading file:", err.message);
+  } catch (error) {
+    console.log("Read Error:", error.message);
     return [];
   }
 }
 
-// write data
 async function write(data) {
   try {
     await fs.writeFile(filePath, JSON.stringify(data, null, 2));
-  } catch (err) {
-    console.error("Error writing file:", err.message);
+  } catch (error) {
+    console.log("Write Error:", error.message);
   }
 }
 
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
+const path = require("path");
 const fileHandler = require("./modules/fileHandler");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Payroll App Server Running");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const employees = await fileHandler.read();
+  res.render("index", { employees });
 });
 
-async function startServer() {
-  const employees = await fileHandler.read();
-  console.log("Employees loaded from JSON:");
-  console.log(employees);
-
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
